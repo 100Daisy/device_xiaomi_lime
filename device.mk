2ba08bc114a31e90d3ecc7e53ed7526182b77495
100Daisy <100Daisy@protonmail.com>
2021-03-18 15:22:37 +0100

    lime: Build Thermal

--- device.mk
+++ device.mk
@@ -367,6 +367,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
